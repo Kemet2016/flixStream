6d8141b98d9a207319544c06f67b79144704fe1e
ahmedmoussa <elyarmuk@hotmail.com>
2020-10-02 22:55:13 -0700

    Unit 2 assignment submission

---
+++ flixStream/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixStream
+//
+//  Created by Ahmed Moussa on 9/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
---
+++ flixStream/UsersViewController.swift
@@ -0,0 +1,104 @@
+//
+//  UsersViewController.swift
+//  flixStream
+//
+//  Created by Ahmed Moussa on 9/23/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class UsersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var moviesTableView: UITableView!
+    
+    
+    var movies = [[String : Any]]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        moviesTableView.dataSource = self
+        moviesTableView.delegate = self
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+            self.movies = dataDictionary["results"] as! [[String: Any]]
+            print(self.movies)
+            self.moviesTableView.reloadData()
+                //print(dataDictionary)
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
+
+           }
+            
+        }
+        task.resume()
+    }
+    
+    
+    
+    // The Table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel!.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        cell.posterView.af.setImage(withURL: posterUrl)
+        return cell
+    }
+    
+    
+    
+    
+    
+
+    
+    // MARK: - Navigation
+
+     //In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        print("Loading up the details screen")
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = moviesTableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        //Pass the selected movie to the details view controller
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        
+        moviesTableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+}
